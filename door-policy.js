96a3ef1d9a4c58cb9ef1a4a4b3d9babe9aac8670
rafacovez <adanescollante@gmail.com>
2023-01-31 15:09:08 -0400

    coded backDoorPassword function

--- door-policy.js
+++ door-policy.js
@@ -43,5 +43,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return frontDoorPassword(word) + ", please"
 }
